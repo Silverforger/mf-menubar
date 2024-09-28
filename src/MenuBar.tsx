55185444c5cb818fff4338386efef3700148bec6
silverforger <borlagdanpierre@gmail.com>
2024-09-29 04:29:13 +0800

    Refactor to use outside navigate

--- src/MenuBar.tsx
+++ src/MenuBar.tsx
@@ -1,12 +1,12 @@
 import "tailwindcss/tailwind.css";
 
 import React, { useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 import useOnClickOutside from "./hooks/useOnClickOutside";
 
 interface MenuBarProps {
     routes: Route[];
+    navigate: Function;
 }
 
 interface Route {
@@ -14,9 +14,7 @@ interface Route {
     label: string;
 }
 
-const MenuBar: React.FC<MenuBarProps> = ({ routes }) => {
-    const navigate = useNavigate();
-
+const MenuBar: React.FC<MenuBarProps> = ({ routes, navigate }) => {
     const menuRef = useRef(null);
 
     const [showMenuBar, setShowMenubar] = useState<boolean>(false);
@@ -32,13 +30,14 @@ const MenuBar: React.FC<MenuBarProps> = ({ routes }) => {
     return (
         <div className="py-2 justify-between select-none asdasd">
             <div
+                id="menubar-uq"
                 ref={menuRef}
                 className="relative w-[80px] h-[40px] bg-[#4F517D] text-white font-[500] rounded-[15px] flex items-center justify-center cursor-pointer"
                 onClick={handleShowMenuBar}
             >
                 Menu
                 {showMenuBar && (
-                    <div className="absolute top-[45px] left-[0] w-[150px] min-h-[200px] p-3 rounded-[15px] bg-[#4F517D]">
+                    <div className="absolute top-[45px] left-[0] w-[150px] min-h-[200px] p-3 rounded-[15px] bg-[#4F517D] flex flex-col gap-[5px]">
                         {routes.map((route: Route, index: number) => (
                             <div
                                 onClick={() => handleNavigate(route.path)}
--- src/placeMenuBar.js
+++ src/placeMenuBar.js
@@ -3,16 +3,14 @@ import { createRoot } from "react-dom/client";
 import MenuBar from "./MenuBar";
 import { BrowserRouter } from "react-router-dom";
 
-const placeMenuBar = (node, routesProp) => {
+const placeMenuBar = (node, routesProp, navigateProp) => {
     if (node === null || node === undefined || typeof node !== "object") return;
 
-    const root = createRoot(node);
+    const root = createRoot(document.getElementById("container"));
     root.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <MenuBar routes={routesProp} />
-            </BrowserRouter>
-        </React.StrictMode>
+        <BrowserRouter>
+            <MenuBar routes={routesProp} navigate={navigateProp} />
+        </BrowserRouter>
     );
 };
 
