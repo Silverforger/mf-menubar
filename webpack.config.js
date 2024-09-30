82bc45d358195d598d079c9208edf3c5c12963b6
silverforger <borlagdanpierre@gmail.com>
2024-10-01 01:27:14 +0800

    Change pathing for prod and dev

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const printCompilationMessage = require('./compilation.config.js');
 
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3002/",
+    publicPath: argv.mode === "development" ? "http://localhost:3002/" : "https://mf-menubar.onrender.com",
   },
 
   resolve: {
